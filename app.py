972c2ba7be68ab7487412b1d5156d47b725a3f83
Monali Patel <monalipatel@Monalis-MacBook-Air.local>
2020-08-11 20:34:00 -0400

    added javascript file

--- app.py
+++ app.py
@@ -21,32 +21,16 @@ def PricePredictor(to_predict_list):
     result = loaded_model.predict(to_predict)
     return result[0]
 
-@app.route('/result',methods = ['POST'])
+@app.route('/result',methods = ['POST'])   
 def result():
     if request.method == 'POST':
         to_predict_list = request.form.to_dict()
         to_predict_list=list(to_predict_list.values())
         to_predict_list = list(map(int, to_predict_list))
         prediction = PricePredictor(to_predict_list)
-            
+        print(prediction)
         return render_template("result.html", prediction=prediction)
 
-
-# @app.route('/', methods=['GET', 'POST'])
-# def main():
-#     if flask.request.method == 'GET':
-#         return(flask.render_template('main.html'))
-#     if flask.request.method == 'POST':
-#         temperature = flask.request.form['temperature']
-#         humidity = flask.request.form['humidity']
-#         windspeed = flask.request.form['windspeed']
-#         input_variables = pd.DataFrame([[temperature, humidity, windspeed]],
-#                                        columns=['temperature', 'humidity', 'windspeed'],
-#                                        dtype=float)
-#         prediction = model.predict(input_variables)[0]
-#         return flask.render_template('main.html',
-                                    #  original_input={'Temperature':temperature,
-                                    #                  'Humidity':humidity,
-                                    #                  'Windspeed':windspeed},
-                                    #  result=prediction,
-                                    #  )
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Home Price Prediction...")
+    app.run()
